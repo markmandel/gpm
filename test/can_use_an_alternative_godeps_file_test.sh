dc86001a17f9ef21d8f8ca74698003116a1d1e3b
Mark Mandel <mark.mandel@gmail.com>
2014-06-20 11:20:48 +1000

    Flags for GPM. -f for specifying the file. -g for what go binary to use and -u to turn off the go get networking option.

--- test/can_use_an_alternative_godeps_file_test.sh
+++ test/can_use_an_alternative_godeps_file_test.sh
@@ -4,7 +4,7 @@ GPM=../bin/gpm
 # Setting up v5.0 of package
 rm -rf $GOPATH/src/github.com/pote/gpm-testing-package
 echo "github.com/pote/gpm-testing-package v6.1" > Godeps.prod
-assert_raises "$GPM install Godeps.prod"
+assert_raises "$GPM install -f Godeps.prod"
 assert "go run go_code.go" "v6.1"
 
 rm Godeps.prod
